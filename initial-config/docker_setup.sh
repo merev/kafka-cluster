b5dc7f7f157e6a9f7f720b5fbac8afd40be8803a
merev <merev.personal@gmail.com>
2023-01-09 02:40:07 +0200

    docker script updated

--- initial-config/docker_setup.sh
+++ initial-config/docker_setup.sh
@@ -8,7 +8,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "* Install Docker ..."
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get update -y && apt-get install -y docker-ce docker-ce-cli containerd.io
 
 echo "* Add vagrant user to docker group ..."
 usermod -aG docker vagrant
