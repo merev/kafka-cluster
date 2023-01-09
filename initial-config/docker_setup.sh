ebeff5992171281f306d0fe6168b9c3340d2a0e6
merev <merev.personal@gmail.com>
2023-01-09 02:37:21 +0200

    docker script updated

--- initial-config/docker_setup.sh
+++ initial-config/docker_setup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 echo "* Add any prerequisites ..."
-apt-get update
-apt-get install -y ca-certificates curl gnupg lsb-release git fontconfig openjdk-11-jre
+apt-get update -y && apt-get install -y ca-certificates curl gnupg lsb-release git fontconfig openjdk-11-jre
 
 echo "* Add Docker repository and key ..."
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "* Install Docker ..."
-apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
 echo "* Add vagrant user to docker group ..."
