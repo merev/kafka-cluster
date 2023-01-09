c2eb3382c95d3a653f38c6a5772a96aeeb90d710
merev <101055426+merev@users.noreply.github.com>
2023-01-09 17:51:19 +0200

    Update kafka-cluster-init.sh

--- shared/post-provision-scripts/kafka-cluster-init.sh
+++ shared/post-provision-scripts/kafka-cluster-init.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 # Execute this script from the ansible workspace directory (/home/vagrant/ansible).
 
-echo "* Run Zookeeper processes ..."
-sudo ansible-playbook kafka-zookeeper-start.yml
-
-echo "* Wait till the Zookepers choose a leader (15 seconds) ..."
-sleep 15
-
-echo "* Run Kafka brokers ..."
-sudo ansible-playbook kafka-broker-start.yml
+echo "* Initialize the kafka cluster ..."
+sudo ansible-playbook kafka-cluster-init.yml
