3e618aa3822a91cb3d606263b8efffab64ef0ec9
merev <101055426+merev@users.noreply.github.com>
2023-01-12 22:27:17 +0200

    Update prometheus_setup.sh

--- initial-config/monitoring/prometheus_setup.sh
+++ initial-config/monitoring/prometheus_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "* Copy the main config file ..."
-mkdir prometheus-config && cp /shared/prometheus-config/prometheus.yml prometheus-config/prometeus.yml
+mkdir prometheus-config && cp /shared/prometheus-config/prometheus.yml prometheus-config/prometheus.yml
 
 echo "* Start the Prometheus container"
 docker container run -d --name prometheus -p 9090:9090 --mount type=bind,source=/home/vagrant/prometheus-config/prometheus.yml,destination=/etc/prometheus/prometheus.yml prom/prometheus 
